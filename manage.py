5bab737611d13f2e2b00419d9b8a30513278692d
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2015-09-03 15:39:08 +0200

    Remove envdir/tests since env vars can be specified in tox.ini

--- manage.py
+++ manage.py
@@ -6,12 +6,7 @@ from {{ project_name }} import get_project_root_path, import_env_vars
 
 
 if __name__ == "__main__":
-    if 'test' in sys.argv:
-        env_dir = os.path.join('envdir', 'tests')
-    else:
-        env_dir = os.path.join('envdir', 'local')
-
-    import_env_vars(os.path.join(get_project_root_path(), env_dir))
+    import_env_vars(os.path.join(get_project_root_path(), 'envdir'))
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.dev")
 
