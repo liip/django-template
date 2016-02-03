37136c5adb603a662716775791f23e358b3ce5c3
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2016-02-03 17:15:02 +0100

    Improve cookiecutter integration

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+
+def install_drifter():
+    os.system('git init .')
+    os.system('curl -sS https://raw.githubusercontent.com/liip/drifter/master/install.sh | /bin/bash')
+
+
+def set_parameter(path, key, value):
+    patched_lines = []
+    parameter_exists = False
+
+    with open(path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if line.startswith('{}:'.format(key)):
+            line = '{key}: "{value}"\n'.format(key=key, value=value)
+            parameter_exists = True
+        patched_lines.append(line)
+
+    if not parameter_exists:
+        patched_lines.append('{key}: "{value}"\n'.format(key=key, value=value))
+
+    with open(path, 'w') as f:
+        f.write(''.join(patched_lines))
+
+
+def patch_parameters(path):
+    set_parameter(path, 'django_pip_requirements', 'requirements/dev.txt')
+    set_parameter(path, 'project_name', '{{ cookiecutter.project_slug }}')
+    set_parameter(path, 'database_name', '{{ cookiecutter.project_slug }}')
+    set_parameter(path, 'hostname', "{{ cookiecutter.project_slug.replace('_', '-') }}.lo")
+    set_parameter(path, 'python_version', '3')
+
+
+def patch_playbook(path):
+    patched_lines = []
+
+    with open(path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if 'role: django' in line or 'role: postgresql' in line:
+            line = line.replace('# -', '-')
+
+        patched_lines.append(line)
+
+    with open(path, 'w') as f:
+        f.write(''.join(patched_lines))
+
+
+def pip_compile(path):
+    with open('/dev/null', 'wb') as f:
+        subprocess.call(['pip-compile', path], stdout=f)
+
+
+if __name__ == '__main__':
+    if '{{ cookiecutter.use_drifter }}' == 'y':
+        install_drifter()
+        patch_parameters('virtualization/parameters.yml')
+        patch_playbook('virtualization/playbook.yml')
+
+    pip_compile('requirements/dev.in')
+    pip_compile('requirements/base.in')
+    pip_compile('requirements/deploy.in')
--- {{cookiecutter.project_slug}}/README.md
+++ {{cookiecutter.project_slug}}/README.md
@@ -4,44 +4,17 @@ Django project template
 To create a new project using this template, do the following (replace
 `myproject` with the name of your project):
 
-    pip install django
-    django-admin.py startproject --template="https://github.com/liip/django-template/archive/master.zip" myproject
+    pip install cookiecutter
+    cookiecutter gh:liip/django-template
 
-Once the project is created, you'll mostly want to add virtualization support
-to be able to use the same setup on every computer. First, make sure you meet
-all the
-[requirements](https://github.com/team-rawbot/rawbot-virtualization#requirements)
-then run the following commands.
-
-    cd myproject
-    git init .
-    curl -sS https://raw.githubusercontent.com/team-rawbot/rawbot-virtualization/master/install.sh | /bin/bash
-
-Start by editing the file `virtualization/parameters.yml` and uncomment the
-following line:
-
-    django_pip_requirements: "dev"
-
-Also make sure to set the following settings to the value you want (again,
-replace myproject with the name of your project):
-
-    project_name: "myproject"
-    database_name: "myproject"
-
-Then edit the file `virtualization/playbook.yml` and uncomment the following
-lines:
-
-    - { role: postgresql }
-    - { role: django }
-
-Finally, run the following commands to get you project up and running:
+Then run the following commands to get you project up and running:
 
     vagrant up
     vagrant ssh
     ./manage.py createsuperuser
     ./manage.py runserver
 
-Go to http://django.lo/ and start hacking!
+Go to http://my-project.lo/ and start hacking!
 
 Deploying your site
 -------------------
