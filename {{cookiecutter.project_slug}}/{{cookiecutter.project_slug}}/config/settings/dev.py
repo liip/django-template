a307f54d82faf4de54f8e5245a2888218df29cdf
Stefan Heinemann <stefan.heinemann@liip.ch>
2017-05-01 11:27:27 +0200

    Add possibility to configure email sending by ENV vars

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/dev.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/dev.py
@@ -1,3 +1,5 @@
+import dj_email_url
+
 from . import get_env_variable
 from .base import *  # noqa
 
@@ -21,3 +23,14 @@ TEMPLATES[0]['OPTIONS']['loaders'] = (  # noqa
 )
 
 LOGGING = {}
+
+EMAIL_URL = get_env_variable('EMAIL_URL', 'console://')
+email_config = dj_email_url.parse(EMAIL_URL)
+EMAIL_FILE_PATH = email_config['EMAIL_FILE_PATH']
+EMAIL_HOST_USER = email_config['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = email_config['EMAIL_HOST_PASSWORD']
+EMAIL_HOST = email_config['EMAIL_HOST']
+EMAIL_PORT = email_config['EMAIL_PORT']
+EMAIL_BACKEND = email_config['EMAIL_BACKEND']
+EMAIL_USE_TLS = email_config['EMAIL_USE_TLS']
+EMAIL_USE_SSL = email_config['EMAIL_USE_SSL']
