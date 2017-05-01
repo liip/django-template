7c196a3251db4f881af26bc420146c42b9e9ae2a
Stefan Heinemann <stefan.heinemann@liip.ch>
2017-05-01 11:27:27 +0200

    Add possibility to configure email sending by ENV vars

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/settings/base.py
@@ -1,6 +1,7 @@
 import os
 
 import dj_database_url
+import dj_email_url
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -264,3 +265,18 @@ LOGGING = {
         },
     },
 }
+
+
+#############
+# E-Mailing #
+#############
+EMAIL_URL = get_env_variable('EMAIL_URL', 'console://')
+email_config = dj_email_url.parse(EMAIL_URL)
+EMAIL_FILE_PATH = email_config['EMAIL_FILE_PATH']
+EMAIL_HOST_USER = email_config['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = email_config['EMAIL_HOST_PASSWORD']
+EMAIL_HOST = email_config['EMAIL_HOST']
+EMAIL_PORT = email_config['EMAIL_PORT']
+EMAIL_BACKEND = email_config['EMAIL_BACKEND']
+EMAIL_USE_TLS = email_config['EMAIL_USE_TLS']
+EMAIL_USE_SSL = email_config['EMAIL_USE_SSL']
