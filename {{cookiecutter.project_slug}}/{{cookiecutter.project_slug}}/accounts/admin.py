6b20b80218db220565ee6d811dcadd09e8f6f35f
Raphaël Snts <r@0x73.ch>
2017-06-04 11:24:42 +0200

    Remove useless class for admin registration

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/accounts/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/accounts/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
-
-
-class UserAdmin(BaseUserAdmin):
-    pass
-
 
 admin.site.register(User, UserAdmin)
