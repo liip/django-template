09420836f19b3ba5322b2bdcbc01c40449324c89
Simon Brulhart <simon.brulhart@liip.ch>
2018-08-27 19:00:25 +0200

    Fix under-indentation in URLConf

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        path('media/<path:path>/', django.views.static.serve,
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        path('media/<path:path>/', django.views.static.serve, {
+            'document_root': settings.MEDIA_ROOT, 'show_indexes': True
+        }),
         path('__debug__/', include(debug_toolbar.urls)),
     ] + staticfiles_urlpatterns() + urlpatterns
