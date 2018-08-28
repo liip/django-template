5af1c419e44bfccebfb81d6ddfdeaa7beee7a483
Raphaël Santos <r@0x73.ch>
2018-08-28 14:58:46 +0200

    Sort imports

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/urls.py
@@ -1,8 +1,8 @@
 import django.views.static
 from django.conf import settings
-from django.urls import include, path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 admin.autodiscover()
