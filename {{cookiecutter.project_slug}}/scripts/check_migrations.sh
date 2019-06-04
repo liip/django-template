9dc4f6ea54d13f631131aa0d7d3e6899d818e3b4
Simon Brulhart <simon.brulhart@liip.ch>
2019-06-04 18:45:34 +0200

    Fix exit status

--- {{cookiecutter.project_slug}}/scripts/check_migrations.sh
+++ {{cookiecutter.project_slug}}/scripts/check_migrations.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo "Checking for missing migrations"
-./manage.py makemigrations --dry-run --check --no-input -v1 || (echo "Missing migrations detected!" && exit 1)
-exit 0
+if ! ./manage.py makemigrations --dry-run --check --no-input -v1; then
+  echo "Missing migrations detected!"
+  exit 1
+fi
