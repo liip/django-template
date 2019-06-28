a0ba75609ca3c3c00e98cb0f8b86a56950aacdf3
Simon Brulhart <simon.brulhart@liip.ch>
2019-06-28 08:11:42 +0200

    Add missing semicolon

--- {{cookiecutter.project_slug}}/scripts/check_migrations.sh
+++ {{cookiecutter.project_slug}}/scripts/check_migrations.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Checking for missing migrations"
-./manage.py makemigrations --dry-run --check --no-input -v1 || { echo "Missing migrations detected!"; exit 1 }
+./manage.py makemigrations --dry-run --check --no-input -v1 || { echo "Missing migrations detected!"; exit 1; }
 exit 0
