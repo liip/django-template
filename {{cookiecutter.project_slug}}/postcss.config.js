f805226963e43dffe3599a98d4005ed7f11adbdd
Benoît Burgener <benoit.burgener@gmail.com>
2020-06-04 14:37:00 +0200

    Use built-in Tailwind CSS Purge functionnality

--- {{cookiecutter.project_slug}}/postcss.config.js
+++ {{cookiecutter.project_slug}}/postcss.config.js
@@ -1,9 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./{{ cookiecutter.project_slug }}/**/*.html'],
-
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -11,6 +5,5 @@ module.exports = {
     require('precss'),
     require('postcss-extend-rule'),
     require('postcss-preset-env'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 };
--- {{cookiecutter.project_slug}}/tailwind.config.js
+++ {{cookiecutter.project_slug}}/tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  purge: {
+    content: ['./{{ cookiecutter.project_slug }}/**/*.html'],
+    options: {},
+  },
   theme: {},
   variants: {},
   plugins: [],
