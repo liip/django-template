f4c40e80196f95f7cb8fc7264c2cbb4bfdf14df9
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-06-23 08:59:07 +0200

    Remove references to tox

--- {{cookiecutter.project_slug}}/scripts/run_tests.sh
+++ {{cookiecutter.project_slug}}/scripts/run_tests.sh
@@ -3,15 +3,10 @@
 
 # don't delete this line, or copy that content and adjust it
 . ./virtualization/drifter/ci/test-header.sh
-
-~/ENV/bin/tox -r
-
-# don't delete this line, or copy that content and adjust it
-. ./virtualization/drifter/ci/test-footer.sh
 {%- elif cookiecutter.virtualization_tool == 'docker' -%}
 #!/bin/sh -e
+{%- endif -%}
 
 ./scripts/check_migrations.sh
 pytest "${@:-{{ cookiecutter.project_slug }}}"
 flake8 {{ cookiecutter.project_slug }}
-{%- endif -%}
