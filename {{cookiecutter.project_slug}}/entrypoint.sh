66ea44a6b8dedcb19da55b3ebfe6e7bc7c8c65ef
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2020-06-23 09:11:44 +0200

    Compile test requirements on first container mount

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -14,6 +14,10 @@ if [ "$INITIAL" = "1" ]; then
 
     pip install -r requirements/dev.txt
 
+    if [ ! -e "requirements/test.txt" ]; then
+        pip-compile requirements/test.in
+    fi
+
     # Wait for the db server to be ready, then run the fixturize command
     while ! (echo > /dev/tcp/db/5432) >/dev/null 2>&1; do echo -n '.'; sleep 1; done;
     echo "Running fixturize..."
