88b2df6dc92de05c84625e095a2cb08b0c001c24
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2020-06-23 17:20:00 +0200

    Fix whitespace fiddling in run_tests

--- {{cookiecutter.project_slug}}/scripts/run_tests.sh
+++ {{cookiecutter.project_slug}}/scripts/run_tests.sh
@@ -1,12 +1,14 @@
-{% if cookiecutter.virtualization_tool == 'drifter' -%}
 #!/bin/bash
+{% if cookiecutter.virtualization_tool == 'drifter' %}
 
 # don't delete this line, or copy that content and adjust it
 . ./virtualization/drifter/ci/test-header.sh
-{%- elif cookiecutter.virtualization_tool == 'docker' -%}
-#!/bin/sh -e
-{%- endif -%}
+{% endif %}
+
+set -e
 
 ./scripts/check_migrations.sh
+
 pytest "${@:-{{ cookiecutter.project_slug }}}"
+
 flake8 {{ cookiecutter.project_slug }}
